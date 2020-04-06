403b0320b831935532a6382244b5d0e15acc10a7
EclecticTaco <calvi5210@gmail.com>
2020-04-06 15:04:26 -0700

    remove foreign key constriants

--- db/postgreSQL.sql
+++ db/postgreSQL.sql
@@ -1,19 +1,21 @@
-DROP DATABASE IF EXISTS soundCloudComments;
+DROP DATABASE IF EXISTS soundcloud;
 
-CREATE DATABASE soundCloudComments;
+CREATE DATABASE soundcloud;
 
-\c soundCloudComments;
+\c soundcloud;
 
 CREATE SCHEMA nimubs; 
 
 DROP
 
 CREATE TABLE "users" (
-  "user_id" SERIAL PRIMARY KEY,
-  "user_name" varchar(20),
-  "location" varchar(30),
-  "image" varchar(300),
-  "followers" integer
+  "user_id" SERIAL PRIMARY KEY, 
+  "user_name" varchar(20), --faker random 
+  "location" varchar(30), --faker random 
+  "image" varchar(300), --faker random image
+  "tracks" integer, --random number
+  "num_of_songs" integer, --random number??
+  "followers" integer --random number
 );
 
 CREATE TABLE "songs" (
@@ -21,24 +23,23 @@ CREATE TABLE "songs" (
   "likes" integer,
   "reposts" integer,
   "followers" integer,
-  "tracks" integer,
   "song_name" varchar(30),
-  "artist" varchar(30),
+  "user_id_songs" varchar(30), -- references user table with user_id
   "length" varchar(10)
 );
 
 CREATE TABLE "comments" (
-  "comment_id" SERIAL PRIMARY KEY,
+  "comment_id" SERIAL PRIMARY KEY, -- num between 1 and 1,000,000
   "text" varchar(300),
-  "user" varchar(30),
-  "song" integer,
-  "reply_id" integer,
+  "user" varchar(30), -- references user_id in users: random num between 1 and 20,000,000
+  "song" integer, -- references song_id in songs: random number between 1 and 1,000,000
+  "reply_id" integer, -- references comment_id of comments if a reply, otherwise NULL to represent parent comment
   "song_timestamp" varchar(10),
   "date" date
 );
 
-ALTER TABLE "songs" ADD FOREIGN KEY ("song_id") REFERENCES "comments" ("song");
+-- ALTER TABLE "songs" ADD FOREIGN KEY ("song_id") REFERENCES "comments" ("song");
 
-ALTER TABLE "users" ADD FOREIGN KEY ("user_id") REFERENCES "comments" ("user");
+-- ALTER TABLE "users" ADD FOREIGN KEY ("user_id") REFERENCES "comments" ("user");
 
-ALTER TABLE "comments" ADD FOREIGN KEY ("comment_id") REFERENCES "comments" ("reply_id");
+-- ALTER TABLE "comments" ADD FOREIGN KEY ("comment_id") REFERENCES "comments" ("reply_id");
