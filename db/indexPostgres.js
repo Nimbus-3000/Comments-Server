55eb172abbc898e0f5ac554148a795407f95d41d
EclecticTaco <calvi5210@gmail.com>
2020-04-10 10:24:25 -0700

    add functions to query ps db

---
+++ db/indexPostgres.js
@@ -0,0 +1,29 @@
+const { Pool, Client } = require('pg');
+
+const client = new Client({
+  user: 'Calvin',
+  host: 'localhost',
+  database: 'soundcloud',
+  port: 5432,
+});
+client.connect();
+
+const getComments = (args, callback) => {
+  console.log('db getComments pinged')
+  const queryStr = 'select * from public.songs INNER JOIN public.users ON public.songs.user_id_songs = public.users.user_id where public.songs.song_id = 8000000;'
+
+  client.query(queryStr, (err, res) => {
+    if (err) {
+      console.log(err.stack);
+    } else {
+      console.log(res.rows[0]);
+    }
+  });
+
+  // client
+  //   .query(queryStr)
+  //   .then(res => console.log(res.rows[0]))
+  //   .catch(e => console.error(e.stack))
+};
+
+module.exports = { getComments };
